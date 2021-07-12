7359325aa1506fd4af7ed71cf182f08aafe2ed76
MoTRona <69713167+MoTRona@users.noreply.github.com>
2021-07-12 18:45:55 +0300

    Still in dev

--- scripts/blocks/turrets/gem.js
+++ scripts/blocks/turrets/gem.js
@@ -12,9 +12,11 @@ const GemSpear = extend(BasicBulletType, {
 });
 GemSpear.hitSize = 8;
 GemSpear.knockback = 120.0;
-GemSpear.damage = 500000;
-GemSpear.speed = 10;
-GemSpear.lifetime = 1000;
+GemSpear.damage = 2.25e5;
+GemSpear.speed = 50;
+GemSpear.pierce = true;
+GemSpear.pierceBuilding = true;
+GemSpear.lifetime = 20;
 GemSpear.hitEffect = E.magicBulletHitSmall;
 GemSpear.despawnEffect = E.magicBulletHitSmall;
 GemSpear.hitColor = C.magicLight.cpy();
@@ -64,9 +66,9 @@ const Gem = extendContent(Turret, "gem", {
 	    this.bars.add("charge",
             func(e =>
 		        new Bar(
-	                prov(() => Core.bundle.get("charge")+": "+e.getPowerCharge()+"/100000"),
-			        prov(() => e.getPowerCharge() == 100000 ? C.rubyLight : C.angelLight),
-			        floatp(() => e.getPowerCharge()/100000)
+	                prov(() => Core.bundle.get("charge")+": "+e.getPowerCharge().toFixed()+"/100000"),
+			        prov(() => e.getPowerCharge() == 1e5 ? C.rubyLight : C.angelLight),
+			        floatp(() => e.getPowerCharge()/1e5)
 	            )
 	        )
         )
@@ -81,6 +83,15 @@ Gem.buildType = () => {
 			
 			return this;
 		},  
+
+        targetPosition(pos){
+            if(!this.hasAmmo() || pos == null) return;
+            
+            this.targetPos.set(Predict.intercept(this, pos, 50));
+            if(this.targetPos.isZero()){
+                this.targetPos.set(pos);
+            }
+        }, 
 		
         acceptItem(source, item){
             this.setPowerCharge(Math.min(this.getPowerCharge()+item.hardness+item.cost, 100000)); 
@@ -90,13 +101,16 @@ Gem.buildType = () => {
         acceptStack(item, amount, source){return 0}, 
         handleItem(source, item){}, 
         handleStack(item, amount, source){},         //I'm sure these methods are very frustrated with their uselessness. Sad
-        effects(){},
+        useAmmo(){},
         peekAmmo(){}, 
         ejectEffects(){}, 
 
         updateShooting(){
             if(!this.charging){
-                this.shoot(GemSpear);
+	            SO.gaussShoot.at(this.x + this.block.tr.x, this.y + this.block.tr.y, 2);
+            	
+	            this.usePower();
+                Time.run(60, () => { this.shoot(GemSpear) })
             }
         }, 
         
@@ -105,12 +119,36 @@ Gem.buildType = () => {
             this.bullet(type, this.rotation + Mathf.range(this.block.inaccuracy));
 	 
             this.recoil = this.block.recoilAmount;
-            this.heat = 1.0;
+            
+            this.effects();
         }, 
 
+        bullet(type, angle){
+            var b = type.create(this, this.team, this.x + this.block.tr.x, this.y + this.block.tr.y, angle, Mathf.random(75000.0, 150000.0), 1.0, 1.0, [8]);
+            
+            let time = b.dst(this.targetPos)/type.speed;
+            for(var i = 0; i < 6; i++) {
+            	var speed1 = (1.0-0.95/6*i);
+            	Time.run(i*8, () => { 
+            	    Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed1, 3 * speed1));
+                });
+                
+            	var speed2 = (0.05+0.95/6*i);
+            	Time.run(time+i*8, () => { 
+            	    Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed2, 3 * speed2));
+                }) 
+            } 
+        },
+        
+        effects(){
+            //effects later
+            
+            
+            this.recoil = this.block.recoilAmount
+        }, 
         
         hasAmmo(){
-            return this.getPowerCharge() == 100000;
+            return this.getPowerCharge() == 1e5;
         }, 
         
 		setPowerCharge(a){
@@ -131,10 +169,10 @@ Gem.consumes.liquid(F.fl("helium-liquid"), 0.5);
 
 F.node(F.fb("duo"), Gem, 
     ItemStack.with(
-	    Items.surgeAlloy, 225000, F.fi("meteorite"), 180000, 
-	    F.fi("palladium"), 195000, Items.titanium, 100000, 
-	    Items.thorium, 115000, F.fi("photonite"), 160000, 
-	    Items.metaglass, 200000, Items.silicon, 185000, 
+	    Items.surgeAlloy, 2.25e5, F.fi("meteorite"), 180000, 
+	    F.fi("palladium"), 1.95e5, Items.titanium, 100000, 
+	    Items.thorium, 1.15e5, F.fi("photonite"), 1.6e5, 
+	    Items.metaglass, 2e5, Items.silicon, 185000, 
 	    F.fi("contritum"), 250000
     ), Seq.with(
 	    new Objectives.Research(F.fi("meteorite")), 
